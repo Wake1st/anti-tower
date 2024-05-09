2bcdb15038fefec7d4a8f237ca0688908db106ec
Wake1st <joeltwakefield@gmail.com>
2024-05-08 19:23:10 -0600

    camera follows player

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,4 @@
-use bevy::{
-    core_pipeline::clear_color::ClearColorConfig, input::common_conditions::input_toggle_active,
-    prelude::*, render::camera::ScalingMode,
-};
+use bevy::{input::common_conditions::input_toggle_active, prelude::*};
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
 
 use bubble::BubblePlugin;
@@ -38,22 +35,5 @@ fn main() {
         )
         .insert_resource(Money(100.0))
         .add_plugins((PlayerPlugin, BubblePlugin, PotionPlugin, GameUI))
-        .add_systems(Startup, setup)
         .run();
-}
-
-fn setup(mut commands: Commands) {
-    let mut camera = Camera2dBundle {
-        camera_2d: Camera2d {
-            clear_color: ClearColorConfig::Custom(Color::MIDNIGHT_BLUE),
-        },
-        ..default()
-    };
-
-    camera.projection.scaling_mode = ScalingMode::AutoMin {
-        min_width: 256.0,
-        min_height: 144.0,
-    };
-
-    commands.spawn(camera);
 }
--- src/player.rs
+++ src/player.rs
@@ -1,12 +1,12 @@
-use bevy::prelude::*;
+use bevy::{core_pipeline::clear_color::ClearColorConfig, prelude::*, render::camera::ScalingMode};
 use bevy_inspector_egui::{prelude::ReflectInspectorOptions, InspectorOptions};
 
 pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, spawn_player)
-            .add_systems(Update, character_movement);
+        app.add_systems(Startup, (spawn_player, spawn_camera))
+            .add_systems(Update, (character_movement, camera_follow).chain());
     }
 }
 
@@ -52,3 +52,51 @@ fn character_movement(
         }
     }
 }
+
+fn spawn_camera(mut commands: Commands) {
+    let mut camera = Camera2dBundle {
+        camera_2d: Camera2d {
+            clear_color: ClearColorConfig::Custom(Color::MIDNIGHT_BLUE),
+        },
+        ..default()
+    };
+
+    camera.projection.scaling_mode = ScalingMode::AutoMin {
+        min_width: 512.0,
+        min_height: 288.0,
+    };
+
+    commands.spawn(camera);
+}
+
+fn camera_follow(
+    player: Query<&Transform, With<Player>>,
+    mut camera: Query<&mut Transform, (With<Camera2d>, Without<Player>)>,
+) {
+    let player_transform = player.single();
+
+    for mut transform in &mut camera {
+        let pos = player_transform.translation;
+        transform.translation.x = pos.x;
+        transform.translation.y = pos.y;
+    }
+}
+
+// fn camera_follow(
+//     player: Query<&Transform, With<Player>>,
+//     mut camera: Query<(&mut Camera, &mut Transform), Without<Player>>,
+// ) {
+//     let Ok(player) = player.get_single() else {
+//         return;
+//     };
+//     let Ok((mut camera, mut camera_transform)) = camera.get_single_mut() else {
+//         return;
+//     };
+
+//     let delta = player.translation - camera.focus;
+
+//     if delta != Vec3::ZERO {
+//         camera.focus = player.translation;
+//         camera_transform.translation += delta;
+//     }
+// }
